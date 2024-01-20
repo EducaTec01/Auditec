e14fa3b23c8e6eef28359e04f4ddf6330b886678
Cesar Andree <105308517+cesarinmoca@users.noreply.github.com>
2024-01-19 17:13:37 -0800

    Responsive Widgets
    When reaching 1000 px as maximum width, widgets will behave in a way that they transform to become two rows and two columns instead of a single row.

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -13,10 +13,10 @@ const Home = () => {
       <div className="homeContainer">
         <Navbar />
         <div className="widgets">
-          <Widget type="user" />
-          <Widget type="order" />
-          <Widget type="earning" />
-          <Widget type="balance" />
+          <Widget className="widget" type="user" />
+          <Widget className="widget" type="order" />
+          <Widget className="widget" type="earning" />
+          <Widget className="widget" type="balance" />
         </div>
         <div className="charts">
           <Featured />
