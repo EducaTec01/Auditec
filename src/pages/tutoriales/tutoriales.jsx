9ccf45f3501aefdf966b56a833965ce0ea0d3b77
Cesar Andree <105308517+cesarinmoca@users.noreply.github.com>
2024-02-05 21:46:30 -0800

    example comments and minus fixes

--- src/pages/tutoriales/tutoriales.jsx
+++ src/pages/tutoriales/tutoriales.jsx
@@ -1,6 +1,7 @@
 import Navbar2 from "../../components/navbar-2/Navbar-2";
 import "./tutoriales.scss"
 import miniatura from "./miniatura-prueba.png"
+import profile from "./profile-picture.png"
 
 const Tutoriales = () => {
     return (
@@ -21,17 +22,80 @@ const Tutoriales = () => {
                         </div>
                     </div>
                     <div className="contenedor">
-                        <p>Archivo.pdf</p>
+                        <p></p>
                     </div>
                 </div>
                 <div className="right">
                     <div className="comentarios">
                         <div className="titulo">
                             <p>Comentarios</p>
-                            <p>Next Video</p>
+                            <p className="next-video">Next Video</p>
                         </div>
                         <div className="container">
-                            <p>Contenedor de comentarios</p>
+                            <div className="comentario">
+                                <div className="izq">
+                                    <div className="img">
+                                        <img className="user-picture" alt="Logo" src={profile} />
+
+                                    </div>
+                                </div>
+                                <div className="der">
+                                    <div className="usuario">
+                                        <p>Usuario</p>
+                                    </div>
+                                    <div className="mensaje">
+                                        <p>Nice video!</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="comentario">
+                                <div className="izq">
+                                    <div className="img">
+                                        <img className="user-picture" alt="Logo" src={profile} />
+
+                                    </div>
+                                </div>
+                                <div className="der">
+                                    <div className="usuario">
+                                        <p>Usuario</p>
+                                    </div>
+                                    <div className="mensaje">
+                                        <p>Excellent Performance</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="comentario">
+                                <div className="izq">
+                                    <div className="img">
+                                        <img className="user-picture" alt="Logo" src={profile} />
+
+                                    </div>
+                                </div>
+                                <div className="der">
+                                    <div className="usuario">
+                                        <p>Usuario</p>
+                                    </div>
+                                    <div className="mensaje">
+                                        <p>Asumecha</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="comentario">
+                                <div className="izq">
+                                    <div className="img">
+                                        <img className="user-picture" alt="Logo" src={profile} />
+
+                                    </div>
+                                </div>
+                                <div className="der">
+                                    <div className="usuario">
+                                        <p>Usuario</p>
+                                    </div>
+                                    <div className="mensaje">
+                                        <p>Cual es el mejor Batman del cine?</p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
