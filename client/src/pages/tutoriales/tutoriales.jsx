256c1b1737f92838c3cf3ab6acb402f4fe1687e2
Cesar Andree <105308517+cesarinmoca@users.noreply.github.com>
2024-03-13 14:08:28 -0700

    video commit

--- client/src/pages/tutoriales/tutoriales.jsx
+++ client/src/pages/tutoriales/tutoriales.jsx
@@ -2,17 +2,17 @@ import Navbar2 from "../../components/navbar-2/Navbar-2";
 import "./tutoriales.scss"
 import miniatura from "./miniatura-prueba.png"
 import profile from "./profile-picture.png"
+import Sidebar from "../../components/sidebar/Sidebar";
 
 const Tutoriales = () => {
     return (
       <body>
             <header>
-                <nav>
-                    <Navbar2 />
-                </nav>
+                
             </header>
             <section className="section2">
                 <div className="left">
+                    <Sidebar />
                     <div className="video">
                         <div className="titulo">
                             <p>Video</p>
@@ -22,9 +22,6 @@ const Tutoriales = () => {
                                 <iframe width="560" height="315" src="https://www.youtube.com/embed/1uOo1ZZIgoU?si=__Ve2nqrfH9dW-yV" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                             </div>
                         </div>
-                    </div>
-                    <div className="contenedor">
-                        
                     </div>
                 </div>
             </section>
